df22046d98b05a54f2e97bfbe0a6b2814eb043dd
Jenny ZHANG <zhangshunhang123@gmail.com>
2020-04-18 16:53:51 +0800

    update Post table

--- src/db/model.js
+++ src/db/model.js
@@ -36,6 +36,18 @@ const Post = seq.define("post",{
     tagId:{
         type:Sequelize.INTEGER,
         allowNull:false
+    },
+    destination:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    startDate:{
+        type:Sequelize.DATE,
+        allowNull:false
+    },
+    endDate:{
+        type:Sequelize.DATE,
+        allowNull:false
     }
 });
 
